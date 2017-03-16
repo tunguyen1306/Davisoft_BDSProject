362a9f27b12e9811995ed5993cf4b610e9f4c346
tunguyen1306 <tunguyen.it.dev@gmail.com>
2017-03-16 17:15:03 +0700

    no message

--- Davisoft_BDSProject.Web/Controllers/AdminCityController.cs
+++ Davisoft_BDSProject.Web/Controllers/AdminCityController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 using Davisoft_BDSProject.Web.Models;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Logical;
+using ServiceStack;
 
 namespace Davisoft_BDSProject.Web.Controllers
 {
@@ -26,22 +29,53 @@ namespace Davisoft_BDSProject.Web.Controllers
         {
             return View();
         }
+        public ActionResult Edit( int id)
+        {
+            var queryData = from city in db.states
+                            join cityTex in db.statetexts on city.name_id equals cityTex.id
+                            where cityTex.language_id.Equals("vi") || city.state_id==id
+                            select new CityModel { City = city, CityText = cityTex };
 
+            return View(queryData.ToList().FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(CityModel  cityModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                state sta = db.states.Find(cityModel.City.state_id);
+                db.Entry(sta).State = EntityState.Modified;
+                sta.Status = cityModel.City.Status;
+                sta.stateCode = cityModel.City.stateCode;
+                statetext statetext = (from data in db.statetexts where data.id==cityModel.CityText.id && data.language_id=="vi" select data).ToList().FirstOrDefault();
+                db.Entry(statetext).State = EntityState.Modified;
+                statetext.text = cityModel.CityText.text;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cityModel);
+        }
         //
         // POST: /Default1/Create
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(state state)
+        public ActionResult Create(CityModel cityModel)
         {
             if (ModelState.IsValid)
             {
-                db.states.Add(state);
+                
+                cityModel.City.CreateDate = DateTime.Now;
+                cityModel.City.ModifyDate = DateTime.Now;
+                cityModel.City.countryCode = "VN";
+                cityModel.City.name_id = 1234;
+                db.states.Add(cityModel.City);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(state);
+            return View();
         }
 
     }
