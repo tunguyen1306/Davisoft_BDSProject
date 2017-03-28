df2293ee88e3ddd4c0d00acf53d4b1cf680f219f
tunguyen1306 <tunguyen.it.dev@gmail.com>
2017-03-28 17:38:51 +0700

    no message

---
+++ WebBDS_Project/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBDS_Project.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //
+        // GET: /Register/
+
+        public ActionResult RegisterCompany()
+        {
+            return View();
+        }
+        public ActionResult RegisterEmployee()
+        {
+            return View();
+        }
+
+    }
+}
