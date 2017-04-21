1839c258bff1cee922bfceda92bb8d4d31227c85
nguyen tan tu <tunguyen.it.dev@gmail.com>
2017-04-21 09:04:50 +0700

    no message

--- WebBDS_Project/App_Start/RouteConfig.cs
+++ WebBDS_Project/App_Start/RouteConfig.cs
@@ -12,12 +12,110 @@ namespace WebBDS_Project
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //Management
+            routes.MapRoute(
+             name: "quan-li-tin-dang",
+             url: "tai-khoan/quan-li-tin-dang",
+             defaults: new { controller = "Management", action = "ListNewOfUser" }
+         );
+            
+            routes.MapRoute(
+             name: "quan-ly-tai-khoan-nha-tuyen-dung",
+             url: "tai-khoan/quan-ly-tai-khoan-nha-tuyen-dung",
+             defaults: new { controller = "Management", action = "ManagementAcountEmployee" }
+         );
+            routes.MapRoute(
+            name: "quan-ly-nha-tuyen-dung",
+            url: "tai-khoan/quan-ly-nha-tuyen-dung",
+            defaults: new { controller = "Management", action = "ManagementCompany" }
+        );
+            routes.MapRoute(
+            name: "nop-tien",
+            url: "tai-khoan/nop-tien",
+            defaults: new { controller = "Management", action = "Payment" }
+        );
+            routes.MapRoute(
+           name: "cam-on",
+           url: "tai-khoan/cam-on",
+           defaults: new { controller = "Management", action = "ThanksApply" }
+       );
+            routes.MapRoute(
+          name: "ho-so-da-luu",
+          url: "tai-khoan/ho-so-da-luu",
+          defaults: new { controller = "Management", action = "YourArchive" }
+      );
+            routes.MapRoute(
+          name: "quen-mat-khau",
+          url: "tai-khoan/quen-mat-khau",
+          defaults: new { controller = "Management", action = "ForgetPass" }
+      ); routes.MapRoute(
+          name: "ung-vien-gui-ho-so",
+          url: "tai-khoan/ung-vien-gui-ho-so",
+          defaults: new { controller = "Management", action = "ApplyArchive" }
+      );
+            //Login
+            routes.MapRoute(
+          name: "dang-nhap",
+          url: "tai-khoan/dang-nhap",
+          defaults: new { controller = "Login", action = "Login" }
+      );
+            routes.MapRoute(
+         name: "trang-dang-nhap",
+         url: "tai-khoan/trang-dang-nhap",
+         defaults: new { controller = "Login", action = "LoginForm" }
+     );
+            //tuyendung
+            routes.MapRoute(
+      name: "trang-chu",
+      url: "tuyen-dung/trang-chu",
+      defaults: new { controller = "Default", action = "Index" }
+  );
+            routes.MapRoute(
+    name: "chi-tiet-tin-tuc",
+    url: "tin-tuc/chi-tiet-tin-tuc",
+    defaults: new { controller = "Default", action = "DetailNews"}
+);
+            routes.MapRoute(
+   name: "chi-tiet-tin",
+   url: "tin-tuc/chi-tiet-tin",
+   defaults: new { controller = "Default", action = "Detail" }
+);
+            routes.MapRoute(
+name: "tiem-kiem",
+url: "tin-tuc/tiem-kiem",
+defaults: new { controller = "Default", action = "Search" }
+);
+            //dang-tin
+            routes.MapRoute(
+name: "dang-tin",
+url: "tin-tuyen-dung/dang-tin",
+defaults: new { controller = "Adverts", action = "AdvertCompany" }
+);
+            //Dang ky
+            routes.MapRoute(
+name: "dang-ky-tuyen-dung",
+url: "dang-ky/dang-ky-tuyen-dung",
+defaults: new { controller = "Register", action = "RegisterCompany" }
+);
+            routes.MapRoute(
+name: "dang-ky-nguoi-tim-viec",
+url: "dang-ky/dang-ky-nguoi-tim-viec",
+defaults: new { controller = "Register", action = "RegisterPersonal" }
+);
+            routes.MapRoute(
+name: "dang-tin-thanh",
+url: "dang-ky/dang-tin-thanh",
+defaults: new { controller = "Register", action = "Thanks" }
+);
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
             );
+        
+          
+
         }
     }
 }
