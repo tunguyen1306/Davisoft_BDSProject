6d58aadfed16fcf6fa00d06e7c2c6ec3aa99474c
minhloc1406 <leminhloc14061989@gmail.com>
2017-05-03 10:10:54 +0700

    no message

---
+++ Davisoft_BDSProject.Web/Validation/BDSBannerValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Davisoft_BDSProject.Domain.Entities;
+using FluentValidation;
+using Resources;
+
+namespace Davisoft_BDSProject.Web.Validation
+{
+    public class BDSBannerValidator : AbstractValidator<BDSBanner>
+    {
+        public BDSBannerValidator()
+        {
+        
+            RuleFor(m => m.Page).NotEmpty().WithMessage(Resource.TheFieldShouldNotBeEmpty);
+            RuleFor(m => m.Position).NotEmpty().WithMessage(Resource.TheFieldShouldNotBeEmpty);
+            RuleFor(m => m.Type).NotEmpty().WithMessage(Resource.TheFieldShouldNotBeEmpty);
+            RuleFor(m => m.Banner).NotEmpty().WithMessage(Resource.TheFieldShouldNotBeEmpty);
+      
+        }
+    }
+}
